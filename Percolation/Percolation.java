e5b9e115cf1a700503951d8c1419913c58c52923
Hojoon Ji <sleeperbus@gmail.com>
2014-02-20 07:10:23 +0900

    bug fixed

--- Percolation/Percolation.java
+++ Percolation/Percolation.java
@@ -1,115 +1,123 @@
 public class Percolation {
-	private WeightedQuickUnionUF uf;
-	private int size;
-	private int[] sitesOpen;
+    private WeightedQuickUnionUF uf;
+    private int size;
+    private int[] sitesOpen;
 
-	// convert cordination to 1D Index.  x, y start from 1
-	private int xyTo1D(int x, int y) {
-		return (x - 1) + (y - 1) * this.size;
-	}
+    public Percolation(int N) {
+        // 2 is for virtual sites, top & bottom
+        this.uf = new WeightedQuickUnionUF((N*N + 2));
+        this.size = N;
+        this.sitesOpen = new int[N*N];
 
-	// check if indices are out of bounds
-	private boolean isBeyoundIndex(int i, int j) {
-		if (i <= 0 || i > this.size) return false;
-		if (j <= 0 || j > this.size) return false;
-		return true;
-	}
+        // connect virtual top to top rows
+        for (int idx = 0; idx < this.size; idx++) {
+            this.uf.union(N*N, idx);
+        }
 
-	private void checkIndicesBounds(int i, int j) {
-		if (i <= 0 || i > this.size) throw new IndexOutOfBoundsException("row i out of bounds");
-		if (j <= 0 || j > this.size) throw new IndexOutOfBoundsException("column j out of bounds");
-	}	
+        for (int idx = N*N - N; idx < N * N; idx++) {
+            this.uf.union(N*N + 1, idx);
+        }
+    }
 
-	private void connectWithNeighbors(int i, int j) {
-		// right
-		if (isBeyoundIndex(i+1, j) && isOpen(i+1, j)) {
+
+    // convert cordination to 1D Index.  x, y start from 1
+    private int xyTo1D(int x, int y) {
+        return (x - 1) + (y - 1) * this.size;
+    }
+
+    // check if indices are out of bounds
+    private boolean isBeyoundIndex(int i, int j) {
+        if (i <= 0 || i > this.size) return false;
+        if (j <= 0 || j > this.size) return false;
+        return true;
+    }
+
+    private void checkIndicesBounds(int i, int j) {
+        if (i <= 0 || i > this.size) 
+            throw new IndexOutOfBoundsException("row i out of bounds");
+        if (j <= 0 || j > this.size) 
+            throw new IndexOutOfBoundsException("column j out of bounds");
+    }   
+
+    private void connectWithNeighbors(int i, int j) {
+        // right
+        if (isBeyoundIndex(i+1, j) && isOpen(i+1, j)) {
 //            StdOut.println("1");
-			this.uf.union(xyTo1D(i, j), xyTo1D(i+1, j));
-		}
-		// left
-		if (isBeyoundIndex(i-1, j) && isOpen(i-1, j)) {
+            this.uf.union(xyTo1D(i, j), xyTo1D(i+1, j));
+        }
+        // left
+        if (isBeyoundIndex(i-1, j) && isOpen(i-1, j)) {
 //            StdOut.println("2");
-			this.uf.union(xyTo1D(i, j), xyTo1D(i-1, j));
-		}
-		// top
-		if (isBeyoundIndex(i, j-1) && isOpen(i, j-1)) {
+            this.uf.union(xyTo1D(i, j), xyTo1D(i-1, j));
+        }
+        // top
+        if (isBeyoundIndex(i, j-1) && isOpen(i, j-1)) {
 //            StdOut.println("3");
-			this.uf.union(xyTo1D(i, j), xyTo1D(i, j-1));
-		}
-		// bottom
-		if (isBeyoundIndex(i, j+1) && isOpen(i, j+1)) {
+            this.uf.union(xyTo1D(i, j), xyTo1D(i, j-1));
+        }
+        // bottom
+        if (isBeyoundIndex(i, j+1) && isOpen(i, j+1)) {
 //            StdOut.println("4");
-			this.uf.union(xyTo1D(i, j), xyTo1D(i, j+1));
-		}
-	}
+            this.uf.union(xyTo1D(i, j), xyTo1D(i, j+1));
+        }
+    }
 
-	public Percolation(int N) {
-		// 2 is for virtual sites, top & bottom
-		this.uf = new WeightedQuickUnionUF((N*N + 2));
-		this.size = N;
-		this.sitesOpen = new int[N*N];
+    // open site (row i, column j) if it is not already
+    public void open(int i, int j) {
+        checkIndicesBounds(i, j);
+        int idx = xyTo1D(i, j);
+        int status = this.sitesOpen[idx];
+//      StdOut.printf("open (%d, %d) => %d\n", i, j, idx);
+        if (status == 0) {
+            this.sitesOpen[idx] = 1;
+        }
+        connectWithNeighbors(i, j);
+    }
 
-		// connect virtual top to top rows
-		for (int idx = 0; idx < this.size; idx++) {
-			this.uf.union(N*N, idx);
-		}
+    // is site (row i, column j) open?
+    public boolean isOpen(int i, int j) {
+        checkIndicesBounds(i, j);
+        if (this.sitesOpen[xyTo1D(i, j)] == 1) return true;
+        return false;
+    }
 
-		for (int idx = N*N - N; idx < N * N; idx++) {
-			this.uf.union(N*N + 1, idx);
-		}
-	}
+    // is site (row i, column j) full?
+    public boolean isFull(int i, int j) {
+        checkIndicesBounds(i, j);
+        if (this.isOpen(i, j) &&
+		this.uf.connected(this.size * this.size, xyTo1D(i, j)))
+            return true;
+        return false;
+    }
 
-	// open site (row i, column j) if it is not already
-	public void open(int i, int j) {
-		checkIndicesBounds(i, j);
-		int idx = xyTo1D(i, j);
-		int status = this.sitesOpen[idx];
-//		StdOut.printf("open (%d, %d) => %d\n", i, j, idx);
-		if (status == 0) {
-			this.sitesOpen[idx] = 1;
-		}
-		connectWithNeighbors(i, j);
-	}
+    // does the system percolate
+    public boolean percolates() {
+        if (this.uf.connected(this.size * this.size, 
+            this.size * this.size + 1)) 
+            return true;
+        return false;
+    }
 
-	// is site (row i, column j) open?
-	public boolean isOpen(int i, int j) {
-		checkIndicesBounds(i, j);
-		if (this.sitesOpen[xyTo1D(i, j)] == 1) return true;
-		return false;
-	}
+    public static void main(String[] args) {
+        int size = 6;
+        int count = 0;
+        int n1, n2;
 
-	// is site (row i, column j) full?
-	public boolean isFull(int i, int j) {
-		if (this.uf.connected(this.size * this.size, xyTo1D(i, j)))
-			return true;
-		return false;
-	}
-
-	// does the system percolate
-	public boolean percolates() {
-		if (this.uf.connected(this.size * this.size, this.size * this.size + 1)) return true;
-		return false;
-	}
-
-	public static void main(String[] args) {
-		int size = 20;
-		int count = 0;
-		int n1, n2;
-
-		Percolation percol = new Percolation(size);
-        while (percol.percolates() == false) {
+        Percolation percol = new Percolation(size);
+        StdOut.println(percol.isFull(1,1));
+        /*
+        while (!percol.percolates()) {
             n1 = StdRandom.uniform(1, size+1);
             n2 = StdRandom.uniform(1, size+1);
-            if (percol.isOpen(n1, n2) == false) {
+            if (!percol.isOpen(n1, n2)) {
                 count++;
-	            StdOut.printf("Open (%d, %d)\n", n1, n2);
+                StdOut.printf("Open (%d, %d)\n", n1, n2);
                 percol.open(n1, n2);
-		        StdOut.println(percol.percolates());
+                StdOut.println(percol.percolates());
             }            
         }
-        StdOut.printf("%d by %d = %d, open sites are %d\n", size, size, size * size, count);
-        System.exit(0);        
-
-		
-	}
+        StdOut.printf("%d by %d = %d, open sites are %d\n", 
+            size, size, size * size, count);
+        */                    
+    }
 }
--- Percolation/PercolationStats.java
+++ Percolation/PercolationStats.java
@@ -1,10 +1,25 @@
 public class PercolationStats {
-	private int gridSize;
-	private int exTimes;
-    private static double percolMean;
-    private static double percolStddev;
-    private static double confidence95 = 1.96;
+    private double percolMean;
+    private double percolStddev;
+    private double confidence95 = 1.96;
+    private int gridSize;
+    private int exTimes;
     private double [] ratio;
+
+    public PercolationStats(int N, int T) {
+        this.gridSize = N;
+        this.exTimes = T;
+        this.ratio = new double[T];
+        int count;
+
+        for (int idx = 0; idx < this.exTimes; idx++) {
+            count = numOfOpenSitesToPercolates(this.gridSize);
+            this.ratio[idx] = Double.valueOf(count)
+                    / Double.valueOf(this.gridSize*this.gridSize);
+        }
+        this.percolMean = StdStats.mean(ratio);
+        this.percolStddev = StdStats.stddev(ratio);
+    }
 
     // return number of opened sites to percolates
     private int numOfOpenSitesToPercolates(int size) {
@@ -12,7 +27,7 @@ public class PercolationStats {
         int openedSitesCount = 0;
 
         Percolation percol = new Percolation(size);
-        while (percol.percolates() == false) {
+        while (!percol.percolates()) {
             n1 = StdRandom.uniform(1, size+1);
             n2 = StdRandom.uniform(1, size+1);
             if (!percol.isOpen(n1, n2)) {
@@ -23,50 +38,51 @@ public class PercolationStats {
         return openedSitesCount;
     }
 
+    // perform T independent computations experiments on N by N grid
 
-	// perform T independent computations experiments on N by N grid
-	public PercolationStats(int N, int T) {
-		this.gridSize = N;
-		this.exTimes = T;
-        this.ratio = new double[T];
-        int count;
-
-        for (int idx = 0; idx < this.exTimes; idx++) {
-            count = numOfOpenSitesToPercolates(N);
-            this.ratio[idx] = Float.valueOf(count)/Float.valueOf(N*N);
-            StdOut.printf("count %d, %d of %d, ratio %f.\n", count, idx+1, T, this.ratio[idx]);
-        }
-        this.percolMean = StdStats.mean(ratio);
-        this.percolStddev = StdStats.stddev(ratio);
-	}
-
-	// sample mean of percolation threshold
-	public double mean() {
+    // sample mean of percolation threshold
+    public double mean() {
         return this.percolMean;
-	}
+    }
 
-	// sample standard deviation of percolation threshold
-	public double stddev() {
+    // sample standard deviation of percolation threshold
+    public double stddev() {
         return this.percolStddev;
-	}
+    }
 
     // returns lower bound of the 95% confidence interval
     public double confidenceLo() {
-        return 0.0;
+        return 
+        (
+            this.percolMean 
+            - (
+                (this.confidence95 * this.percolStddev) 
+                / Math.sqrt(this.exTimes)
+                )
+        );
     }
 
     // returns uppper bound of the 95% confidence interval
     public double confidenceHi() {
-        return 0.0;
+        return (this.percolMean 
+            + ((this.confidence95 * this.percolStddev) 
+                / Math.sqrt(this.exTimes)));
     }    
 
     // test client
     public static void main(String[] args) {
-        PercolationStats test = new PercolationStats(20, 100);
+        int p1 = Integer.parseInt(args[0]);
+        int p2 = Integer.parseInt(args[1]);
+
+        PercolationStats test = new PercolationStats(p1, p2);
         double mean = test.mean();
         double stddev = test.stddev();
+        double confiHi = test.confidenceHi();
+        double confiLo = test.confidenceLo();
 
         StdOut.printf("mean\t\t\t: %f\n", mean);
         StdOut.printf("stddev\t\t\t: %f\n", stddev);
+        StdOut.println(confiLo);
+        StdOut.println(confiHi);
     }
 }
