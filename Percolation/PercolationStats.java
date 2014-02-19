6f50c46d6cbd66990a17e32bfe177ec35e48dcf6
sleeperbus <sleeperbus@gmail.com>
2014-02-19 11:57:02 +0900

    New file

---
+++ Percolation/PercolationStats.java
@@ -0,0 +1,31 @@
+public class PercolationStats {
+	// perform T independent computations experiments on N by N grid
+	public PercolationStats(int N, int T) {
+
+	}
+
+	// sample mean of percolation threshold
+	public double mean() {
+
+	}
+
+	// sample standard deviation of percolation threshold
+	public double stddev() {
+
+	}
+
+    // returns lower bound of the 95% confidence interval
+    public double confidenceLo() {
+
+    }
+
+    // returns uppper bound of the 95% confidence interval
+    public double confidenceHi() {
+        
+    }    
+
+    // test client
+    public static void main(String[] args) {
+        
+    }
+}
