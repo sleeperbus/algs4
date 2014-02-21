2ca2ae198c1f52486121c50b8662586cf62f5fed
Hojoon Ji <sleeperbus@gmail.com>
2014-02-21 12:09:49 +0900

    before fixing backwash bug

--- Percolation/Percolation.java
+++ Percolation/Percolation.java
@@ -8,29 +8,26 @@ public class Percolation {
         this.uf = new WeightedQuickUnionUF((N*N + 2));
         this.size = N;
         this.sitesOpen = new int[N*N];
-
-        // connect virtual top to top rows
-        for (int idx = 0; idx < this.size; idx++) {
-            this.uf.union(N*N, idx);
-            StdOut.printf("(%d, %d) connected: %b\n", 
-				N*N, idx, this.uf.connected(N*N, idx));
-        }
-
-        for (int idx = N*N - N; idx < N * N; idx++) {
-            this.uf.union(N*N + 1, idx);
-        }
     }
 
+    // connect to top
+    private void connectToTop(int i, int j) {
+       this.uf.union(this.size * this.size, xyTo1D(i, j)); 
+    }
+
+    // connect to bottom
+    private void connectToBottom(int i, int j) {
+       this.uf.union(this.size * this.size + 1, xyTo1D(i, j)); 
+    }
 
     // convert cordination to 1D Index.  x, y start from 1
     private int xyTo1D(int x, int y) {
-		int idx = (y - 1) + (x - 1) * this.size;
-		StdOut.printf("(%d, %d) to %d\n", x, y, idx);
-		return idx;
+        int idx = (y - 1) + (x - 1) * this.size;
+        return idx;
     }
 
     // check if indices are out of bounds
-    private boolean isBeyoundIndex(int i, int j) {
+    private boolean isBoundIndex(int i, int j) {
         if (i <= 0 || i > this.size) return false;
         if (j <= 0 || j > this.size) return false;
         return true;
@@ -45,24 +42,29 @@ public class Percolation {
 
     private void connectWithNeighbors(int i, int j) {
         // right
-        if (isBeyoundIndex(i+1, j) && isOpen(i+1, j)) {
-//            StdOut.println("1");
+        if (isBoundIndex(i+1, j) && isOpen(i+1, j)) {
             this.uf.union(xyTo1D(i, j), xyTo1D(i+1, j));
         }
         // left
-        if (isBeyoundIndex(i-1, j) && isOpen(i-1, j)) {
-//            StdOut.println("2");
+        if (isBoundIndex(i-1, j) && isOpen(i-1, j)) {
             this.uf.union(xyTo1D(i, j), xyTo1D(i-1, j));
         }
         // top
-        if (isBeyoundIndex(i, j-1) && isOpen(i, j-1)) {
-//            StdOut.println("3");
+        if (isBoundIndex(i, j-1) && isOpen(i, j-1)) {
             this.uf.union(xyTo1D(i, j), xyTo1D(i, j-1));
         }
         // bottom
-        if (isBeyoundIndex(i, j+1) && isOpen(i, j+1)) {
-//            StdOut.println("4");
+        if (isBoundIndex(i, j+1) && isOpen(i, j+1)) {
             this.uf.union(xyTo1D(i, j), xyTo1D(i, j+1));
+        }
+
+        // if (i, j) is first row or last row then connect
+        // to virtual top or bottom node.
+        if (i == 1) {
+            this.connectToTop(i, j);
+        }
+        if (i == this.size) {
+            this.connectToBottom(i, j);
         }
     }
 
@@ -71,7 +73,6 @@ public class Percolation {
         checkIndicesBounds(i, j);
         int idx = xyTo1D(i, j);
         int status = this.sitesOpen[idx];
-//      StdOut.printf("open (%d, %d) => %d\n", i, j, idx);
         if (status == 0) {
             this.sitesOpen[idx] = 1;
         }
@@ -88,9 +89,7 @@ public class Percolation {
     // is site (row i, column j) full?
     public boolean isFull(int i, int j) {
         checkIndicesBounds(i, j);
-        if (this.isOpen(i, j) 
-            && this.uf.connected(this.size * this.size, xyTo1D(i, j)))
-            return true;
+        if (this.uf.connected(this.size * this.size, xyTo1D(i, j))) return true;
         return false;
     }
 
@@ -103,27 +102,12 @@ public class Percolation {
     }
 
     public static void main(String[] args) {
-        int size = 6;
+        int size = 1;
         int count = 0;
         int n1, n2;
+        boolean b1;
 
         Percolation percol = new Percolation(size);
-		percol.open(1, 6);
-		StdOut.println(percol.isOpen(1, 6));
-        StdOut.println(percol.isFull(1, 6));
-        /*
-        while (!percol.percolates()) {
-            n1 = StdRandom.uniform(1, size+1);
-            n2 = StdRandom.uniform(1, size+1);
-            if (!percol.isOpen(n1, n2)) {
-                count++;
-                StdOut.printf("Open (%d, %d)\n", n1, n2);
-                percol.open(n1, n2);
-                StdOut.println(percol.percolates());
-            }            
-        }
-        StdOut.printf("%d by %d = %d, open sites are %d\n", 
-            size, size, size * size, count);
-        */                    
+        percol.open(1, 1);
     }
 }
