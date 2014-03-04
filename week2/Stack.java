bdd1bb73408d413a47ac2b2acd4f3cebea94a77d
Hojoon Ji <sleeperbus@gmail.com>
2014-03-04 23:19:50 +0900

    ...

--- week2/Stack.java
+++ week2/Stack.java
@@ -1,43 +1,123 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class Stack<Item> {
-	private Node first = null;
+public class Stack<Item> implements Iterable<Item> {
+	private int N;				// size of stack
+	private Node<Item> first;	// top of stack
 
-	private class Node {
-		Item item;
-		Node next;
+	// helper linked list class
+	private static class Node<Item> {
+		private Item item;
+		private Node<Item> next;
 	}
 
-	private class ListIterator implements Iterator<Item> {
-		private Node current = first;
+	/**
+	 * Initializes an empty stack.
+	 */
+	public Stack() {
+		first = null;
+		N = 0;
+	}
+
+	/**
+	 * Is this stack empty?
+	 * @return true if this stack is empty; false otherwise
+	 */
+	public boolean isEmpty() { return first == null; }
+
+	/**
+	 *  Return the number of items in the stack.
+	 * @return the number of items in the stack.
+	 */
+	public int size() { return N; }
+
+	/**
+	 * Adds the item to this stack.
+	 * @param item the item to add
+	 */
+	public void push(Item item) {
+		Node<Item> oldfirst = first;
+		first = new Node<Item>();
+		first.item = item;
+		first.next = oldfirst;
+		N++;
+	}
+
+	/**
+	 * Removes and returns the item most recently added to this stack.
+	 * @return the item most recently added
+	 * @throws java.util.NoSuchElementException if this stack is empty
+	 */
+	public Item pop() {
+		if (isEmpty()) throw new NoSuchElementException("Stack Underflow");
+		Item item = first.item;
+		first = first.next;
+		N--;
+		return item;
+	}
+
+	/**
+	 * Returns (but does not remove) the item most recently added to this stack.
+	 * @return the item most recently added to this stack.
+	 * @throws java.util.NoSuchElementException if this stack is empty
+	 */
+	public Item peek() {
+		if (isEmpty()) throw new NoSuchElementException("Stack Underflow");
+		return first.item;
+	}
+
+
+	/**
+	 * Returns a string representation of this stack.
+	 * @return the sequence of items in the stack in LIFO order, separated by
+	 * spaces
+	 */
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		for (Item item : this)
+			s.append(item + " ");
+		return s.toString();
+	}
+
+
+	/**
+	 * Returns an iterator to this stack that iterates through the items in LIFO order.
+	 * @return an iterator to this stack that iterates through the items in LIFO order.
+	 */
+	public Iterator<Item> iterator() {
+		return new ListIterator<Item>(first);
+	}
+
+
+	// an iterator, doesn't implements remove() since it's optional
+	private class ListIterator<Item> implements Iterator<Item> {
+		private Node<Item> current;
+
+		public ListIterator(Node<Item> first) {
+			current = first;
+		}
 
 		public boolean hasNext() { return current != null; }
-		public void remove() { /* not suppported */ }
+		public void remove() { throw new UnsupportedOperationException(); }
+
 		public Item next() {
+			if (!hasNext()) throw new NoSuchElementException();
 			Item item = current.item;
 			current = current.next;
 			return item;
 		}
 	}
 
-	public Iterator<Item> iterator() {
-		return new ListIterator();
-	}
-
-
-
-	public boolean isEmpty() { return first == null; }
-
-	public void push(Item item) {
-		Node oldfirst = first;
-		first = new Node();
-		first.item = item;
-		first.next = oldfirst;
-	}
-
-	public Item pop() {
-		Item item = first.item;
-		first = first.next;
-		return item;
+	/**
+	 * Unit tests the <tt>Stack</tt> data type.
+	 */
+	public static void main(String[] args) {
+		Stack<String> s = new Stack<String>();
+		while (!StdIn.isEmpty()) {
+			String item = StdIn.readString();
+			if (!item.equals("-")) s.push (item);
+			else if (!s.isEmpty()) StdOut.print(s.pop() + " ");
+		}
+		StdOut.println("(" + s.size() + " left on stack)");
 	}
 }
