c1eceb0b7b9d0a36c91db45303b70c13e80a6fbe
sleeperbus <sleeperbus@gmail.com>
2014-03-10 17:49:39 +0900

    ...

--- week2/TestRandomizedQue.java
+++ week2/TestRandomizedQue.java
@@ -29,7 +29,7 @@ public class TestRandomizedQue {
 		assertEquals(2, randQue.size());
 	}
 
-	public void testEnqueue() {
+	public void testEnqueueAndDequeue() {
 		StdOut.println("enqueue test");
 		RandomizedQueue<Integer> randQue = new RandomizedQueue<Integer>();
 
@@ -54,6 +54,52 @@ public class TestRandomizedQue {
 		StdOut.println("dequeue number: " + randQue.dequeue());
 	}
 
+	public void testSample() {
+		StdOut.println("sample test");
+		RandomizedQueue<Integer> randQue = new RandomizedQueue<Integer>();
+
+		randQue.enqueue(10);
+		randQue.enqueue(20);
+		randQue.enqueue(30);
+		randQue.enqueue(40);
+		randQue.enqueue(50);
+		randQue.enqueue(60);
+		randQue.enqueue(70);
+		randQue.enqueue(80);
+		randQue.enqueue(90);
+
+		StdOut.printf("rand: %d\n", randQue.sample());
+		StdOut.printf("rand: %d\n", randQue.sample());
+		StdOut.printf("rand: %d\n", randQue.sample());
+		StdOut.printf("rand: %d\n", randQue.sample());
+		StdOut.printf("rand: %d\n", randQue.sample());
+		StdOut.printf("rand: %d\n", randQue.sample());
+		StdOut.printf("rand: %d\n", randQue.sample());
+		StdOut.printf("rand: %d\n", randQue.sample());
+		StdOut.printf("rand: %d\n", randQue.sample());
+		StdOut.printf("rand: %d\n", randQue.sample());
+	}
+
+	public void testIterator() {
+		StdOut.println("iterator test");
+		RandomizedQueue<Integer> randQue = new RandomizedQueue<Integer>();
+
+		randQue.enqueue(10);
+		randQue.enqueue(20);
+		randQue.enqueue(30);
+		randQue.enqueue(40);
+		randQue.enqueue(50);
+		randQue.enqueue(60);
+		randQue.enqueue(70);
+		randQue.enqueue(80);
+		randQue.enqueue(90);
+
+		for (int n : randQue) {
+			StdOut.printf("rand num: %n\n", n);
+		}
+
+	}
+
 
 	public static void main(String[] args) {
 
@@ -61,7 +107,9 @@ public class TestRandomizedQue {
 
 		test.testIsEmpty();
 		test.testSize();
-		test.testEnqueue();
+		test.testEnqueueAndDequeue();
+		test.testSample();
+		test.testIterator();
 	}
 
 
