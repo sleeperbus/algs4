21b157f7ec34ad83dea39ddf3355406506316d09
sleeperbus <sleeperbus@gmail.com>
2014-03-13 09:02:30 +0900

    New file: Selection.java

---
+++ week3/Selection.java
@@ -0,0 +1,97 @@
+import java.util.Comparator;
+
+public class Selection {
+    private Selection() { }
+
+    /**
+     * Rearranges the array in ascending order, using the natural order.
+     * @param a the array to be sorted.
+     */
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i+1; j < N; j++) {
+                if (less(a[j], a[min])) min = j;
+            }
+            exch(a, i, min);
+            assert isSorted(a, 0, i);
+        }
+        assert isSorted(a);
+    }
+
+    /**
+     * Rearranges the array in asceding order, using a comparator
+     * @param a the array
+     * @param c the comparator specifying the order
+     */
+    public static void sort(Object[] a, Comparator c) {
+        int N = a.length;
+        for (int i = 0; i < N) {
+            int mint = i;
+            for (int j = i+1; j < N; j++) {
+                if (less(c, a[j], a[min])) min = j;
+            }
+            exch(a, i, min)
+            assert isSorted(a, c, 0, i);
+        }
+        assert isSorted(a, c);
+    }
+
+    /***************************************************************************
+     * Helper sorting functions
+     **************************************************************************/
+
+    // is v < w ?
+    private static boolean less(Comparable v, Comparable w) {
+        return (v.compareTo(w) < 0);
+    }
+
+    // is v < w ?
+    private static boolean less(Comparator c, Object v, Object w) {
+        return (c.compare(v, w) < 0);
+    }
+
+    // exchange a[i] and a[j]
+    private static void exch(Object[], int i, int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+
+    /**************************************************************************
+     * Check if array is sorted - useful for debugging
+     **************************************************************************/
+
+    // is the array a[] sorted?
+    private static boolean isSorted(Comparable[] a) {
+        return isSorted(a, 0, a.length - 1);
+    }
+
+    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++) {
+            if (less(a[i], a[i-1])) return false;
+        }
+        return true;
+    }
+
+    private static boolean isSorted(Object[] a, Comparator c, int lo, int c) {
+        for (int i = lo + 1; i <= hi; i++)
+            if (less(c, a[i], a[i-1])) return false;
+        return true;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.println(a[i]);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        String[] a = StdIn.readAllString();
+        Selection.sort(a);
+        show(a);
+    }
+}
